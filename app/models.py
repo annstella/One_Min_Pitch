19b136ab2c106c51755f0efa4f4a3ed403896f4b
Annstella <annstellawangui@gmail.com>
2018-09-09 22:05:04 +0300

    email column for users email addresses

--- app/models.py
+++ app/models.py
@@ -13,3 +13,4 @@ class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True, index = True)
