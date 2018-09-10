2ae19bc7dc5814f7db02a3cdef19804e3873908a
Annstella <annstellawangui@gmail.com>
2018-09-10 12:27:47 +0300

    added SQLALCHEMY database uri to config.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,20 +1,52 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from ..models import Group,Line,Comment,Vote
+from .forms import LineForm,CommentForm,GroupForm,UpvoteForm
+from flask_login import login_required,current_user
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    title = 'Home'
 
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
+    groups = Group.get_groups()
+
+    return render_template('index.html', title = title, groups=groups )
+
+@main.route('/group/new', methods=['GET','POST'])
+@login_required
+def new_group():
 
     '''
-    View movie page function that returns the movie details page and its data
+    View new group route function that returns a page with a form to create a category
     '''
-    return render_template('movie.html',id = movie_id)
+
+    form = GroupForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        new_group = Group(name=name)
+        new_group.save_group()
+
+        return redirect(url_for('.index'))
+
+    title = 'New Group'
+    return render_template('new_group.html', group_form = form)
+
+
+@main.route('/group/<int:id>')
+def group(id):
+
+    '''
+    View group route function that returns a list of pitches in the route and allows a user to create a pitch for the selected route
+    '''
+    group = Group.query.get(id)
+
+    if group is None:
+        abort(404)
+
+    lines = Line.get_lines(id)
+    title = f'{group.name} page'
+
+    return render_template('group.html', title=title, group=group, lines=lines)
--- config.py
+++ config.py
@@ -1,17 +1,44 @@
 import os
 
 class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
- class ProdConfig(Config):
-    
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://annstella:sterun@localhost/watchlist'
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://annstella:sterun@localhost/one_min_pitch'
 
 class DevConfig(Config):
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://annstella:sterun@localhost/one_min_pitch'
+
     DEBUG = True
 
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
 }
