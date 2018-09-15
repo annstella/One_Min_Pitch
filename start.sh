6f85ee7b0e3bed00eeb58b1e945c029fab04f80a
Annstella <annstellawangui@gmail.com>
2018-09-15 15:02:42 +0300

    deploying to heroku

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ export SECRET_KEY=stella
 export MAIL_USERNAME=annstellawangui12@gmail.com
 export MAIL_PASSWORD=stellakagai12
 
+DATABASE_URL='postgres://ievrjhgosfftvn:0a868cb53b4844921f0969843b864b2af2b2fbe27f1b2cdd5c835c45f0629b4f@ec2-23-23-253-106.compute-1.amazonaws.com:5432/d81rqb9qv2qvig'
 
 python3.6 manage.py server 
